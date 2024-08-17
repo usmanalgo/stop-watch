3194eee68201cee49dd27fdd6c72c364502d3b52
usmanalgo <ua691754@gmail.com>
2024-08-18 00:41:35 +0500

    task completed

---
+++ app.js
@@ -0,0 +1,22 @@
+// console.log('hello');
+// const head = document.querySelector("#timer")
+
+// var timer;
+// var num = 0
+
+// function startTimer (){
+//     timer = setInterval(function (){
+    
+//         num += 1
+//         head.innerHTML = num
+        
+//     } , 1000)
+// }
+
+
+// function stopTimer(){
+//     clearInterval(timer)
+// }
+// function reset() {
+    
+// }
